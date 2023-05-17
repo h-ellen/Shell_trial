d98fcff96398c9e9462348abc161a727a86e4850
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-05-17 08:15:31 -0700

    updated loop

---
+++ shellLoop.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+* main - function method of our shell
+* Return: alwayz zero
+*/
+
+int main(void)
+{
+	char command[100];
+
+	while (1)
+	{
+		printf("user$ ");
+		fgets(command, sizeof(command), stdin);
+
+		/*Check for the exit command*/
+		if (strcmp(command, "exit\n") == 0)
+		{
+			exit(0);
+		}
+
+		/*Execute the command*/
+		system(command);
+	}
+
+	return (0);
+}
