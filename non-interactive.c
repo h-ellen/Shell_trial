a330b374e468940d9a5f10484fa7d27a658bce3c
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-05-17 08:32:42 -0700

    not interactive

---
+++ non-interactive.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * not_interactive - unix command line interpreter
+ *
+ * Return: void
+ */
+void not_interactive(void)
+{
+ char *line;
+ char **args;
+ int status = -1;
+
+ do {
+  line = read_stream();
+  args = split_line(line); /* tokenize line */
+  status = commands(args);
+  /* avoid memory leaks */
+  free(line);
+  free(args);
+  /* exit with status */
+  if (status >= 0)
+  {
+   exit(status);
+  }
+ } while (status == -1);
+}
+
+/**
+ *read_stream - read a line from the stream
+ *
+ *Return: pointer to read string
+ */
+char *read_stream(void)
+{
+ int bufsize = 1024;
+ int i = 0;
+ char *line = malloc(sizeof(char) * bufsize);
+ int character;
+
+ if (line == NULL)
+ {
+  fprintf(stderr, "allocation error in read_stream");
+  exit(EXIT_FAILURE);
+ }
+ while (1)
+ {
+  character = getchar(); /* read first char from stream */
+  if (character == EOF)
+  {
+   free(line);
+   exit(EXIT_SUCCESS);
+  }
+  else if (character == '\n')
+  {
+   line[i] = '\0';
+   return (line);
+  }
+  else
+  {
+   line[i] = character;
+  }
+  i++;
+  if (i >= bufsize)
+  {
+   bufsize += bufsize;
+   line = realloc(line, bufsize);
+   if (line == NULL)
+   {
+    fprintf(stderr, "reallocation error in read_stream");
+    exit(EXIT_FAILURE);
+   }
+  }
+  break;
+ }
+}
